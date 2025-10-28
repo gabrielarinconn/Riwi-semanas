f508a5b2d04e6bc515ced3983c25ab1038582afc
menelikph <menelikdev@gmail.com>
2025-10-28 11:32:07 -0500

    primer comentario

---
+++ 123.py
@@ -0,0 +1,7 @@
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+print(f" hola mi nombre es {nombre} tengo {edad} años")
+if edad >= 18:
+    print("Soy mayor de edad")
+else:
+    print("Soy menor de edad")
